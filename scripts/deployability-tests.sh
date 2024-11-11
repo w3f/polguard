c6828df1e44bc912c5ed0b50a070fa9c25d7aab2
Alessio Onori <alessio.onori@gmail.com>
2024-11-11 15:44:15 +0100

    add helm charts and helmfile.d

---
+++ scripts/deployability-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source /scripts/common.sh
+source /scripts/bootstrap-helm.sh
+
+
+run_tests() {
+    echo Running tests...
+
+    wait_pod_ready redis-master
+    wait_pod_ready chain-watcher
+    wait_pod_ready matrix
+}
+
+main(){
+
+    /scripts/build-helmfile.sh
+
+    run_tests
+}
+
+main
